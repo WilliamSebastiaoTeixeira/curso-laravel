cc0a4e8525da868a1dddefd7e2d9a252eaabaf3a
William Sebastiao Teixeira <williansebastiao99@gmail.com>
2021-09-07 20:45:25 -0300

    auth e css

--- resources/views/admin/_layouts/template.blade.php
+++ resources/views/admin/_layouts/template.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titlePage') - {{config('app.name')}}</title>
+    <link rel="stylesheet" href="{{url('css/app.css')}}">
 </head>
-<body>
+<body class="bg-gray-50">
+    {{--
     <a href="{{ route('admin.posts') }}">Posts</a>
     <a href="{{ route('admin.create') }}">Create New</a>
+    --}}
     <hr>
-    <div class="content">
+    <div class="container mx-auto py-8">
         @yield('content')
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,18 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::any('/posts/filter', [PostController::class, 'filter'])->name('admin.filter');
-
-Route::put('/posts/edit/{id}', [PostController::class, 'change'])->name('admin.change');
-
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('admin.edit');
-
-Route::post('/posts', [PostController::class, 'store'])->name('admin.store');
-
-Route::get('/posts/create', [PostController::class, 'create'])->name('admin.create');;
-
-Route::delete('/posts/{id}', [PostController::class,'delete'])->name('admin.delete');
-
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('admin.show');
-
-Route::get('/posts', [PostController::class, 'posts'])->name('admin.posts');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function(){
+    Route::any('/posts/filter', [PostController::class, 'filter'])->name('admin.filter');
+    Route::put('/posts/edit/{id}', [PostController::class, 'change'])->name('admin.change');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('admin.edit');
+    Route::post('/posts', [PostController::class, 'store'])->name('admin.store');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('admin.create');;
+    Route::delete('/posts/{id}', [PostController::class,'delete'])->name('admin.delete');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('admin.show');
+    Route::get('/posts', [PostController::class, 'posts'])->name('admin.posts');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');    
+}); 
 
 require __DIR__.'/auth.php';
